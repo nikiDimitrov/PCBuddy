90d7c3f80db27dd116317f69968210a934232094
nikiDimitrov <niki.dimitrov0@abv.bg>
2026-02-15 15:57:01 +0200

    Made first run view repsonsive with scrollbar and minwidth and minheight

--- PCBuddy/Views/FirstRunView.xaml.cs
+++ PCBuddy/Views/FirstRunView.xaml.cs
@@ -1,21 +1,18 @@
-using Microsoft.UI.Xaml;
 using PCBuddy.ViewModels;
-
+using WinUIEx;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace PCBuddy.Views;
-
-/// <summary>
-/// An empty window that can be used on its own or navigated to within a Frame.
-/// </summary>
-public sealed partial class FirstRunView : Window
+namespace PCBuddy.Views
 {
-    internal FirstRunViewModel ViewModel { get; set; }
-
-    public FirstRunView()
+    public sealed partial class FirstRunView : WindowEx
     {
-        InitializeComponent();
-        ViewModel = new FirstRunViewModel();
+        internal FirstRunViewModel ViewModel { get; set; }
+
+        public FirstRunView()
+        {
+            InitializeComponent();
+            ViewModel = new FirstRunViewModel();
+        }
     }
 }
