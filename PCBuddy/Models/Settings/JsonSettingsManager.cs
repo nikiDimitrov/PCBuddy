146e24206187f986bc3b1ca5bf93336137bb90d3
nikiDimitrov <niki.dimitrov0@abv.bg>
2026-04-09 00:46:17 +0300

    changed path of json config file

--- PCBuddy/Models/Settings/JsonSettingsManager.cs
+++ PCBuddy/Models/Settings/JsonSettingsManager.cs
@@ -1,8 +1,6 @@
 ﻿using Newtonsoft.Json;
 using PCBuddy.Models.ComputerInfo;
-using PCBuddy.Models.Enums;
 using System;
-using System.CodeDom.Compiler;
 using System.IO;
 
 namespace PCBuddy.Models.Settings
@@ -21,7 +19,7 @@ namespace PCBuddy.Models.Settings
 
             var settingsSerialized = JsonConvert.SerializeObject(jsonSettingsObject);
 
-            var folder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
             var appFolder = Path.Combine(folder, "PCBuddy");
             Directory.CreateDirectory(appFolder);
